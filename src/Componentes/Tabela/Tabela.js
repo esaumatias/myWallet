fca8f5f33ea7d41efe40db20c16c9f9d2cf5f1f2
Esau Matias <esaufreirematias@gmail.com>
2022-03-05 12:10:04 -0300

    feat: (Tabela) - implementa campos da tabela

--- src/Componentes/Tabela/Tabela.js
+++ src/Componentes/Tabela/Tabela.js
@@ -1,9 +1,41 @@
 import React, { useContext } from 'react';
 import AppContext from '../../Context/AppContext';
+import { Table, Row, Col, Container, Spinner } from 'react-bootstrap';
 
 function Tabela() {
   return (
-    <h1>TABELA</h1>
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
   )
 }
 

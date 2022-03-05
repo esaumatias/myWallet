28143ad02525de62317d977f0be8905744c9fa51
Esau Matias <esaufreirematias@gmail.com>
2022-03-05 12:01:03 -0300

    feat: (Formulario) - implementa inputs no formulario

--- src/Componentes/Formulario/Formulario.js
+++ src/Componentes/Formulario/Formulario.js
@@ -4,29 +4,56 @@ import { Form, Row, Col, Container, Spinner } from 'react-bootstrap';
 
 function Formulario() {
   const { currencie } = useContext(AppContext);
+  const pagamento = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+  const tag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+
   return (
     <section>
       <Container>
         <Form>
           <Row>
-            <Col>
+           <Form.Group as={Col} controlId="formGridState">
               <Form.Label>Valor:</Form.Label>
-              <Form.Control placeholder="Valor" type="number"/>
-            </Col>
-            <Col>
+              <Form.Control type="number" placeholder="Valor" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
               <Form.Label>Descrição:</Form.Label>
-              <Form.Control placeholder="Descrição" />
-            </Col>
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>Moeda:</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                  {currencie ? (
-                    currencie.map((value, index) =>(
-                      <option key={ index }>{value}</option>
-                  ))
-                  ): <Spinner animation="border" />}
-                </Form.Select>
-              </Form.Group>
+              <Form.Control type="text" placeholder="Descrição" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Moeda:</Form.Label>
+              <Form.Select>
+                {currencie ? (
+                  currencie.map((value, index) =>(
+                    <option key={ index }>{value}</option>
+                ))
+                ): <Spinner animation="border" />}
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Método de pagamento:</Form.Label>
+              <Form.Select>
+                {pagamento ? (
+                  pagamento.map((value, index) =>(
+                    <option key={ index }>{value}</option>
+                ))
+                ): <Spinner animation="border" />}
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Tag:</Form.Label>
+              <Form.Select>
+                {tag ? (
+                  tag.map((value, index) =>(
+                    <option key={ index }>{value}</option>
+                ))
+                ): <Spinner animation="border" />}
+              </Form.Select>
+            </Form.Group>
           </Row>
         </Form>
       </Container>

5103dd1dcf5b638f5bd0eb0b92809c21feb3bfc3
Esau Matias <esaufreirematias@gmail.com>
2022-03-12 11:22:11 -0300

    feat: (Login) - implementa inputs de email e senha

--- src/Page/Login/Login.js
+++ src/Page/Login/Login.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
+import React, {useContext} from 'react';
+import AppContext from '../../Context/AppContext';
+import { Form, Container, Button } from 'react-bootstrap';
 
 
 function Home() {
+  const { setIsLooading } = useContext(AppContext);
   return (
-    <div>
+    <Container style={{height: '90vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+      <h1>My Wallet</h1>
       <Form>
         <Form.Group className="mb-3" controlId="formGroupEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Senha" />
         </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
-    </div>
+    </Container>
   )
 }
 
